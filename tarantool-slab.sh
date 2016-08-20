a39ac12fabe88861e00c295cc8b4f5459d0f22fe
Aleksey Naumov <pushok.leha@gmail.com>
2016-08-20 16:30:28 +0300

    fix get value if name same

--- tarantool-slab.sh
+++ tarantool-slab.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "box.slab.info()" |  tarantoolctl connect $1 2>/dev/null |grep $2 |  awk -F':' '{print $2}' |sed -e"s/[ |\%]//g"
+echo "box.slab.info()" |  tarantoolctl connect $1 2>/dev/null |grep "$2:" | head -n 1| awk -F':' '{print $2}'  |sed -e"s/[ |\%]//g"
